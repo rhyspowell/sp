3c2f8e483e3792d284ab418e8363a158b44b2048
Rhys Powell <rhys@rhyspowell.com>
2024-07-13 16:49:26 +0000

    quick build operator subscription yaml

--- scripts/build_operator.sh
+++ scripts/build_operator.sh
@@ -5,5 +5,22 @@ read -p "Continue? (Y/N): " confirm && [[ $confirm == [yY] || $confirm == [yY][e
 read -p "Enter operator name: " operator
 
 manifest=`oc get packagemanifests $operator -o json`
+channel=`jq -r '.status.channels[].name' <<< $manifest`
+currentCSV=`jq -r '.status.channels[].currentCSV' <<< $manifest`
+catalogSource=`jq -r '.status.catalogSource' <<< $manifest`
+namespace=`jq -r '.status.catalogSourceNamespace' <<< $manifest`
 
-echo $manifest
+cat<<EOF> operator_subscription.yaml
+apiVersion: operators.coreos.com/v1alpha1
+kind: Subscription
+metadata:
+  name: $operator
+  namespace: $namespace
+spec:
+  channel: $channel
+  installPlanApproval: Automatic
+  name: $operator
+  source: $catalogSource 
+  sourceNamespace: $namespace
+  startingCSV: $currentCSV
+EOF
